a33dc41a5c4b61374d368c7e6784a51c14de3971
Gargi Singh <gargi.singh@outlook.com>
2022-01-03 13:12:42 -0500

    Added Multiply.java

---
+++ TestProject/src/main/java/com/practice/git/TestProject/Multiply.java
@@ -0,0 +1,12 @@
+package com.practice.git.TestProject;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a=4;
+        int b=2;
+        System.out.println("The product is "+(a*b));
+	}
+
+}
