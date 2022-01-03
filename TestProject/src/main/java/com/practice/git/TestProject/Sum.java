a1643a3c124c3378a9125a2853e17b2b1e4545f6
Gargi Singh <gargi.singh@outlook.com>
2022-01-03 12:36:13 -0500

    initial message

---
+++ TestProject/src/main/java/com/practice/git/TestProject/Sum.java
@@ -0,0 +1,15 @@
+package com.practice.git.TestProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class Sum 
+{
+    public static void main( String[] args )
+    {
+        int a=5;
+        int b=6;
+        System.out.println(a+b);
+    }
+}
